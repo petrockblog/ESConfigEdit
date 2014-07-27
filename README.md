859da762adbe3aa41406f4211c1034f34b37b38c
Florian <info@petrockblock.com>
2014-07-27 11:11:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# ESConfigEdit
+
+This program provides the functions for setting or removing system configurations of [EmulationStation](www.emulationstation.org), a graphical front-end.
+
+
+## Installation
+
+To install this tool, you can download the current release directly from the repository with the command 
+```bash
+git clone https://github.com/petrockblog/ESConfigEdit.git
+```
+This downloads the current repository into the folder `ESConfigEdit`.
+
+
+## Usage
+
+Here is the generic usage information that is also provided from command line:
+```bash
+usage: esconfedit.py [-h] [-f FULLNAME] [-n NAME] [-d DIRECTORY]
+                     [-e EXTENSION] [-c COMMAND] [-p PLATFORM] [-t THEME]
+                     {set,remove} inputfile outputfile
+
+Sets or removes system information from system configuration files of
+EmulationStation (see emulationstation.org).
+
+positional arguments:
+  {set,remove}          Sets the mode
+  inputfile             path and name of input XML file
+  outputfile            path and name of output XML file
+
+optional arguments:
+  -h, --help            show this help message and exit
+
+optional arguments, system-specific::
+  -f FULLNAME, --fullname FULLNAME
+                        full name of the system
+  -n NAME, --name NAME  name of the system
+  -d DIRECTORY, --directory DIRECTORY
+                        path of the ROMs of the system
+  -e EXTENSION, --extension EXTENSION
+                        Extensions of the ROMs
+  -c COMMAND, --command COMMAND
+                        Command to start the emulator with a ROM
+  -p PLATFORM, --platform PLATFORM
+                        Platform name for EmulationStation
+  -t THEME, --theme THEME
+                        Theme name for EmulationStation
+```
+
+The program can run in various __modes__:
+
+1. __Set mode__: In this mode the program reads _sourcefile_, adds the given system information to it and writes the result to _outputfile_. If a system with the same name already exists, the existing system information are changed.
+2. __Remove mode__: In this mode the program reads _sourcefile_, removes the system with the given name, and writes the result to _outputfile_. If the system does not exist, _outputfile_ will have the same content as _inputfile_.
+
+
+## Example
+
+### Setting System Configurations
+
+Here is an exemplary function call to add/edit a system. Please note that the backslashes:
+
+```
+python esconfedit.py -f "Super Nintendo" \
+-n "snes2" \
+-d "~/RetroPie/roms/snes" \
+-e ".smc .sfc .fig .swc .SMC .SFC .FIG .SWC" \
+-c "/opt/retropie/supplementary/runcommand/runcommand.sh 4 \"/opt/retropie/emulators/RetroArch/installdir/bin/retroarch -L /opt/retropie/emulatorcores/pocketsnes-libretro/libretro.so --config /opt/retropie/configs/all/retroarch.cfg --appendconfig /opt/retropie/configs/snes/retroarch.cfg %ROM%\"" \
+-p "snes" \
+-t "snes" \
+set es_systems.xml new_es_systems.xml
+```
+
+
+### Removing System Configurations
+
+Here is an exemplary function call to remove a system:
+
+```
+python esconfedit.py -n "snes2" remove es_systems.xml new_es_systems.xml
+```
+
+If you have any comments or ideas ofr enhancements feel free to post a comment or issue at [https://github.com/petrockblog/ESConfigEdit](https://github.com/petrockblog/ESConfigEdit).
+
---
+++ esconfedit.py
@@ -0,0 +1,275 @@
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+import argparse
+import os.path
+import sys
+
+
+class Systemlist(object):
+
+    REPLENT_TOXML = 0
+    REPLENT_FROMXML = 1
+
+    def __init__(self):
+        """ Initializes the system as empty list """
+        super(Systemlist, self).__init__()
+        self.__systemlist = []
+
+    def __iter__(self):
+        self.__currentIndex = 0
+        if len(self.__systemlist) > 0:
+            return self
+        else:
+            raise StopIteration
+
+    def next(self):
+        if self.__currentIndex < len(self.__systemlist):
+            self.__currentIndex += 1
+            return self.__systemlist[self.__currentIndex - 1]
+        else:
+            raise StopIteration
+
+    def loadSystems(self, sourcefile):
+        self.__sourcefile = sourcefile
+        if not os.path.isfile(sourcefile):
+            print "Cannot find input file", sourcefile
+            sys.exit(1)
+
+        self.__transformEntities(sourcefile, Systemlist.REPLENT_TOXML)
+        self.__tree = ET.parse(self.__sourcefile)
+        self.__transformEntities(sourcefile, Systemlist.REPLENT_FROMXML)
+        self.__root = self.__tree.getroot()
+        for system in self.__root.findall('system'):
+            self.__systemlist.append(Systementry(system.find('fullname').text,
+                                                 system.find('name').text,
+                                                 system.find('path').text,
+                                                 system.find('extension').text,
+                                                 system.find('command').text,
+                                                 system.find('platform').text,
+                                                 system.find('theme').text))
+
+    def saveSystems(self, targetfile):
+        newroot = ET.Element("systemList")
+        for system in self.__systemlist:
+            system.addToTree(newroot)
+
+        outputfile = open(targetfile, 'w')
+        outputfile.write(self.__prettify(newroot))
+        outputfile.close()
+        self.__transformEntities(targetfile, Systemlist.REPLENT_FROMXML)
+
+    def existsSystem(self, searchname):
+        entry = self.__findSystem(searchname)
+        if entry is None:
+            return None
+        else:
+            return entry
+
+    def setSystem(self, systementry):
+        existingSystem = self.__findSystem(systementry.getName())
+        if existingSystem is None:
+            self.__systemlist.append(systementry)
+        else:
+            index = self.__systemlist.index(existingSystem)
+            self.__systemlist[index] = systementry
+
+    def removeSystem(self, systemname):
+        existingSystem = self.__findSystem(systemname)
+        if existingSystem is not None:
+            self.__systemlist.remove(existingSystem)
+
+    def __findSystem(self, searchname):
+        for system in self.__systemlist:
+            if system.getName() == searchname:
+                return system
+        return None
+
+    def __prettify(self, elem):
+        """Return a pretty-printed XML string for the Element.
+        """
+        rough_string = ET.tostring(elem, 'utf-8')
+        reparsed = minidom.parseString(rough_string)
+        return reparsed.toprettyxml(indent="  ")
+
+    def __transformEntities(self, sourcefile, direction):
+        if direction == Systemlist.REPLENT_TOXML:
+            replacements = {'&&': '&amp;&amp;'}
+        elif direction == Systemlist.REPLENT_FROMXML:
+            replacements = {'&amp;&amp;': '&&', '&quot;': '"'}
+
+        with open(sourcefile, "r") as workfile:
+            data = workfile.read()
+            workfile.close()
+        for src, target in replacements.iteritems():
+            data = data.replace(src, target)
+        outfile = open(sourcefile, 'w')
+        outfile.write(data)
+        outfile.close()
+
+
+class Systementry(object):
+
+    ENABLEDTRUE = "True"
+    ENABLEDFALSE = "False"
+
+    """docstring for Systementry"""
+
+    def __init__(self, fullname, name, path, extension, command, platform,
+                 theme):
+        super(Systementry, self).__init__()
+        self.__fullname = fullname
+        self.__name = name
+        self.__path = path
+        self.__extension = extension
+        self.__command = command
+        self.__platform = platform
+        self.__theme = theme
+
+    def __str__(self):
+        return str(self.__name)
+
+    def __eq__(self, other):
+        return self.__name == other.getName()
+
+    def addToTree(self, root):
+        newnode = ET.SubElement(root, 'system')
+        newsubnode = ET.SubElement(newnode, "fullname")
+        newsubnode.text = self.__fullname
+        newsubnode = ET.SubElement(newnode, "name")
+        newsubnode.text = self.__name
+        newsubnode = ET.SubElement(newnode, "path")
+        newsubnode.text = self.__path
+        newsubnode = ET.SubElement(newnode, "extension")
+        newsubnode.text = self.__extension
+        newsubnode = ET.SubElement(newnode, "command")
+        newsubnode.text = self.__command
+        newsubnode = ET.SubElement(newnode, "platform")
+        newsubnode.text = self.__platform
+        newsubnode = ET.SubElement(newnode, "theme")
+        newsubnode.text = self.__theme
+
+    def getFullname(self):
+        return self.__fullname
+
+    def getName(self):
+        return self.__name
+
+    def getPath(self):
+        return self.__path
+
+    def getExtension(self):
+        return self.__extension
+
+    def getCommand(self):
+        return self.__command
+
+    def getPlatform(self):
+        return self.__platform
+
+    def getTheme(self):
+        return self.__theme
+
+    def setFullname(self, fullname):
+        self.__fullname = fullname
+        return True
+
+    def setName(self, name):
+        self.__name = name
+        return True
+
+    def setPath(self, path):
+        self.__path = path
+        return True
+
+    def setExtension(self, extension):
+        self.__extension = extension
+        return True
+
+    def setCommand(self, command):
+        self.__command = command
+        return True
+
+    def setPlatform(self, platform):
+        self.__platform = platform
+        return True
+
+    def setTheme(self, theme):
+        self.__theme = theme
+        return True
+
+
+def checkArguments(args):
+    result = True
+    if args.mode == "set":
+        if args.fullname == None or args.name == None or args.directory == None or args.extension == None or args.command == None or args.platform == None or args.theme == None:
+            result = False
+    elif args.mode == "remove":
+        if args.name == None:
+            result = False
+    return result
+
+
+class Toolparser(object):
+
+    def __init__(self):
+        super(Toolparser, self).__init__()
+        self.__parser = argparse.ArgumentParser(
+            description='Sets or removes system information from system configuration files of EmulationStation (see emulationstation.org).')
+        self.__parser.add_argument("mode", help="Sets the mode",
+                                   choices=["set", "remove"])
+        self.__parser.add_argument(
+            "inputfile", help="path and name of input XML file")
+        self.__parser.add_argument(
+            "outputfile", help="path and name of output XML file")
+        group = self.__parser.add_argument_group(
+            'optional arguments, system-specific:')
+        group.add_argument("-f", "--fullname", help="full name of the system")
+        group.add_argument("-n", "--name", help="name of the system")
+        group.add_argument(
+            "-d", "--directory", help="path of the ROMs of the system")
+        group.add_argument("-e", "--extension", help="Extensions of the ROMs")
+        group.add_argument("-c", "--command",
+                           help="Command to start the emulator with a ROM")
+        group.add_argument("-p", "--platform",
+                           help="Platform name for EmulationStation")
+        group.add_argument("-t", "--theme",
+                           help="Theme name for EmulationStation")
+
+    def parse_args(self):
+        return self.__parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    parser = Toolparser()
+    args = parser.parse_args()
+
+    if args.mode == "set":
+        if checkArguments(args):
+            systemlist = Systemlist()
+            systemlist.loadSystems(args.inputfile)
+            entry = Systementry(args.fullname,
+                                args.name,
+                                args.directory,
+                                args.extension,
+                                args.command,
+                                args.platform,
+                                args.theme)
+            systemlist.setSystem(entry)
+            systemlist.saveSystems(args.outputfile)
+            print "Successfully saved to file", args.outputfile
+        else:
+            print "System arguments are incomplete."
+
+    elif args.mode == "remove":
+        if checkArguments(args):
+            systemlist = Systemlist()
+            systemlist.loadSystems(args.inputfile)
+            systemlist.removeSystem(args.name)
+            systemlist.saveSystems(args.outputfile)
+            print "Removed system " + str(args.name) + " from " +\
+                str(args.inputfile)
+        else:
+            print "System name is missing as argument."
+
+    sys.exit(0)
